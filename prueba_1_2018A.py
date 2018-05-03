9cf43290e829f3b73ca5445536da389b2c6f9eb3
SANMH <38759820+SANMH@users.noreply.github.com>
2018-05-02 20:03:00 -0500

    Add files via upload

--- prueba_1_2018A.py
+++ prueba_1_2018A.py
@@ -2,12 +2,12 @@
 """
 Created on Wed May  2 18:22:13 2018
 
-@author: JonathanAndres
+@author: Fernando Sanmartin
 """
 
 
-def sumar(a, b):
-    return a + b
+def Volumen_Cubo(a):
+    return a * a * a
  
 def restar(a, b):
     return a - b
@@ -15,16 +15,16 @@ def restar(a, b):
 def multiplicar(a, b):
     return a * b;
  
-num1 = input("Num1: ")
-num2 = input("Num2: ")
+num1 = input("Ingrese Arista: ")
+
  
-print("Opciones\n1.- Sumar\n2.- Restar\n3.- Multiplicar")
+print("Opciones\n1.- Volumen Cubo\n2.- Restar\n3.- Multiplicar")
  
-operaciones = { '1': sumar, '2': restar, '3': multiplicar}
+operaciones = { '1': Volumen_Cubo, '2': restar, '3': multiplicar}
  
 seleccion = input('Escoge una: ')
 try:
-    resultado = operaciones[seleccion](int(num1), int(num2))
+    resultado = operaciones[seleccion](int(num1))
     print (resultado)
 except:
     print("Esa no vale")
