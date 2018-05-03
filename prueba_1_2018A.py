6cc52fb2869671c40cf76c33904b000f4b24ffdb
SANMH <38759820+SANMH@users.noreply.github.com>
2018-05-02 21:40:36 -0500

    Add files via upload

--- prueba_1_2018A.py
+++ prueba_1_2018A.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed May  2 21:28:54 2018
 
+@author: Fernando Sanmartin
+"""
+
+# -*- coding: utf-8 -*-
+"""
 """
 import math
 
@@ -13,33 +19,34 @@ def Volumen_Piramide_Triangular(a, b): # (area de la base * altura)/3
 def Volumen_Esfera(a): # 4/3 * Pi * r^3
     return 4/3*math.pi*(a*a*a)
  
-print("Opciones\n1.- Cubo\n2.- Piramide Base Triangular \n3.- Esfera")
+print("Opciones:\n1.- Cubo\n2.- Piramide Base Triangular \n3.- Esfera")
 
 
 operaciones = { '1': Volumen_Cubo, '2': Volumen_Piramide_Triangular, '3': Volumen_Esfera}
  
-seleccion = input('Escoge una: ')
+seleccion = input('Escoge una figura: ')
 
 if seleccion=='1':
-    print('Usted esta en el Cubo')
+    print('Se calcula el volumen del Cubo')
     num1 = input("Ingrese Arista: ")
     resultado = operaciones[seleccion](int(num1))
-    print(resultado)
+    
+    print("El volumen es: ", resultado, "metros cubicos")
 
 elif seleccion=='2':
-    print('Usted esta en la Piramide Base Triangular')
+    print('Se calcula el volumen de la Piramide Base Triangular')
     num1 = input("Ingrese Altura: ")
     num2 = input("Ingrese base: ")
     resultado = operaciones[seleccion](int(num1),int(num2))
-    print(resultado)
+    
+    print("El volumen es: ", resultado, "metros cubicos")
 
 elif seleccion=='3':
-    print('Usted esta en la esfera')
+    print('Se calcula el volumen de la Esfera')
     num1 = input("Ingrese el Radio: ")
     resultado = operaciones[seleccion](int(num1))
-    print(resultado)
+    
+    print("El volumen es: ", resultado, "metros cubicos")
 
 else:
-    print("Esa no vale")
-
-    
+    print("Esa opcion no es valida")
