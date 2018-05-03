a016d48f31c0f8522554b2e0796baac05a98b7c9
SANMH <38759820+SANMH@users.noreply.github.com>
2018-05-02 20:22:14 -0500

    Add files via upload

--- prueba_1_2018A.py
+++ prueba_1_2018A.py
@@ -6,23 +6,19 @@ Created on Wed May  2 18:22:13 2018
 """
 
 
-def Volumen_Cubo(a):
+def Volumen_Cubo(a): # arista al cubo
     return a * a * a
  
-def restar(a, b):
+def Volumen_Piramide_Triangular(a, b): # (area de la base * altura)/3 
     return a - b
  
-def multiplicar(a, b):
+def Volumen_Esfera(a, b): # 4/3 * Pi * r^3
     return a * b;
  
+print("Opciones\n1.- Cubo\n2.- Piramide Base Triangular \n3.- Esfera")
 
 
- 
-print("Opciones\n1.- Volumen Cubo\n2.- Restar\n3.- Multiplicar")
-
-
- 
-operaciones = { '1': Volumen_Cubo, '2': restar, '3': multiplicar}
+operaciones = { '1': Volumen_Cubo, '2': Volumen_Piramide_Triangular, '3': Volumen_Esfera}
  
 seleccion = input('Escoge una: ')
 
