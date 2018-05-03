c3be6cdd4a7d90a57da9bfc33795d843cdb8cc53
SANMH <38759820+SANMH@users.noreply.github.com>
2018-05-02 19:02:38 -0500

    Add files via upload

---
+++ prueba_1_2018A.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  2 18:22:13 2018
+
+@author: JonathanAndres
+"""
+
+
+def sumar(a, b):
+    return a + b
+ 
+def restar(a, b):
+    return a - b
+ 
+def multiplicar(a, b):
+    return a * b;
+ 
+num1 = input("Num1: ")
+num2 = input("Num2: ")
+ 
+print("Opciones\n1.- Sumar\n2.- Restar\n3.- Multiplicar")
+ 
+operaciones = { '1': sumar, '2': restar, '3': multiplicar}
+ 
+seleccion = input('Escoge una: ')
+try:
+    resultado = operaciones[seleccion](int(num1), int(num2))
+    print (resultado)
+except:
+    print("Esa no vale")
