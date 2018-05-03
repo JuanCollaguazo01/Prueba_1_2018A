4bab1fd54d27091781cb6897d6544e984e2f274e
victoromar96 <38759789+victoromar96@users.noreply.github.com>
2018-05-02 22:29:35 -0500

    Add files via upload
    Ejercicio completo con menú iterativo

--- prueba_1_2018A.py
+++ prueba_1_2018A.py
@@ -1,52 +1,77 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May  2 21:28:54 2018
-
-@author: Fernando Sanmartin, Juan Collahuaso, Omar Espin
-"""
-
-# -*- coding: utf-8 -*-
-"""
-"""
+import os
 import math
 
-def Volumen_Cubo(a): # arista al cubo
+
+def Volumen_Cubo(a):  # arista al cubo
+    print ('el volumen del cubo es: ')
     return a * a * a
- 
-def Volumen_Piramide_Triangular(a, b): # (area de la base * altura)/3 
-    return (a * b)/3
- 
-def Volumen_Esfera(a): # 4/3 * Pi * r^3
-    return 4/3*math.pi*(a*a*a)
- 
-print("Opciones:\n1.- Cubo\n2.- Piramide Base Triangular \n3.- Esfera")
 
 
-operaciones = { '1': Volumen_Cubo, '2': Volumen_Piramide_Triangular, '3': Volumen_Esfera}
- 
-seleccion = input('Escoge un Cuerpo Geometrico: ')
+def Volumen_Piramide_Triangular(a, b):  # (area de la base * altura)/3
+    print('el volumen de la piramide triangular es: ')
+    return (a * b) / 3
 
-if seleccion=='1':
-    print('Se calcula el volumen del Cubo')
-    num1 = input("Ingrese Arista: ")
-    resultado = operaciones[seleccion](int(num1))
-    
-    print("El volumen es: ", resultado, "metros cubicos")
 
-elif seleccion=='2':
-    print('Se calcula el volumen de la Piramide Base Triangular')
-    num1 = input("Ingrese Altura: ")
-    num2 = input("Ingrese base: ")
-    resultado = operaciones[seleccion](int(num1),int(num2))
-    
-    print("El volumen es: ", resultado, "metros cubicos")
+def Volumen_Esfera(a):  # 4/3 * Pi * r^3
+    print('el volumen de la esfera es: ')
+    return 4 / 3 * math.pi * (a * a * a)
 
-elif seleccion=='3':
-    print('Se calcula el volumen de la Esfera')
-    num1 = input("Ingrese el Radio: ")
-    resultado = operaciones[seleccion](int(num1))
-    
-    print("El volumen es: ", resultado, "metros cubicos")
+operaciones = {'1': Volumen_Cubo, '2': Volumen_Piramide_Triangular, '3': Volumen_Esfera}
 
-else:
-    print("Esa opcion no es valida")
+
+
+
+def menu():
+
+    os.system('cls')  # Función que limpia la pantalla y muestra nuevamente el menu
+    print (" ")
+    print("Calculo de volumen de figuras geometricas")
+    print("1 - Cubo")
+    print("2 - Piramide Base Triangular")
+    print("3 - Esfera")
+    print("4 - salir")
+
+
+
+
+while True:
+
+    menu() # Mostramos el menu
+
+
+    print("")
+    opcionMenu = input("inserta un numero para elegir la opcion que desea: ") # solicitamos una opción al usuario
+    seleccion = input('ingrese el mismo numero correspondiente al que usted deseo : ') #selecciona el arreglo que declaramos y guarda las funciones, da el valor
+
+    if opcionMenu == "1":
+        print("")
+        if seleccion == '1':
+            print('Usted esta en el Cubo')
+            num1 = input("Ingrese Arista: ")
+            resultado = operaciones[seleccion](int(num1))
+            print(resultado)
+
+    elif opcionMenu == "2":
+        print("")
+
+        if seleccion == '2':
+            print('Usted esta en la Piramide Base Triangular')
+            num1 = input("Ingrese Altura: ")
+            num2 = input("Ingrese base: ")
+            resultado = operaciones[seleccion](int(num1), int(num2))
+            print(resultado)
+
+    elif opcionMenu == "3":
+        print("")
+
+        if seleccion == '3':
+            print('Usted esta en la esfera')
+            num1 = input("Ingrese el Radio: ")
+            resultado = operaciones[seleccion](int(num1))
+            print(resultado)
+
+    elif opcionMenu == "4":
+        break
+    else:
+        print("")
+        input("seleccion incorrecta...\npulsa una tecla para continuar")
